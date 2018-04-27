b5752d9f96fa7a25e9e6b878a2ac5a7b10da27d2
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-04-27 16:35:45 +0900

    feat: divide borads

---
+++ components/BoardsColumn.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-flex xs12 sm4>
+    <v-card style="height: 100%; overflow: scroll;">
+      <v-card-title>
+        <div class="title">Something Name</div>
+      </v-card-title>
+      <hr class="my-1">
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card v-for="(item, index) of items" :key="index">
+              <v-card-title class="pt-1 pl-2 pb-1">
+                <div class="subheading">{{ item.title }}</div>
+              </v-card-title>
+              <v-card-text class="px-2 pt-1">
+                {{ item.subtitle }}
+              </v-card-text>
+              <div class="mb-3" v-if="index + 1 < items.length" :key="index"></div>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'borads-column',
+  data () {
+    return {
+      items: [
+        {
+          title: 'aiueo',
+          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
+        },
+        {
+          title: 'abcde',
+          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
+        },
+        {
+          title: 'kakuk',
+          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
+        },
+        {
+          title: 'sasisuseso',
+          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
+        }
+      ]
+    }
+  }
+}
+</script>
--- pages/boards.vue
+++ pages/boards.vue
@@ -15,75 +15,7 @@
       <v-card color="shades" height="800">
         <v-container grid-list-md text-xs-center class="height-100">
           <v-layout row wrap class="height-100">
-            <v-flex xs12 sm4>
-              <v-card style="height: 100%; overflow: scroll;">
-                <v-card-title>
-                  <div class="title">Something Name</div>
-                </v-card-title>
-                <hr class="my-1">
-                <v-container grid-list-md text-xs-center>
-                  <v-layout row wrap>
-                    <v-flex xs12>
-                      <v-card v-for="(item, index) of items" :key="index">
-                        <v-card-title class="pt-1 pl-2 pb-1">
-                          <div class="subheading">{{ item.title }}</div>
-                        </v-card-title>
-                        <v-card-text class="px-2 pt-1">
-                          {{ item.subtitle }}
-                        </v-card-text>
-                        <div class="mb-3" v-if="index + 1 < items.length" :key="index"></div>
-                      </v-card>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-flex>
-            <v-flex xs12 sm4>
-              <v-card style="height: 100%; overflow: scroll;">
-                <v-card-title>
-                  <div class="title">Something Name</div>
-                </v-card-title>
-                <hr class="my-1">
-                <v-container grid-list-md text-xs-center>
-                  <v-layout row wrap>
-                    <v-flex xs12>
-                      <v-card v-for="(item, index) of items" :key="index">
-                        <v-card-title class="pt-1 pl-2 pb-1">
-                          <div class="subheading">{{ item.title }}</div>
-                        </v-card-title>
-                        <v-card-text class="px-2 pt-1">
-                          {{ item.subtitle }}
-                        </v-card-text>
-                        <div class="mb-3" v-if="index + 1 < items.length" :key="index"></div>
-                      </v-card>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-flex>
-            <v-flex xs12 sm4>
-              <v-card style="height: 100%; overflow: scroll;">
-                <v-card-title>
-                  <div class="title">Something Name</div>
-                </v-card-title>
-                <hr class="my-1">
-                <v-container grid-list-md text-xs-center>
-                  <v-layout row wrap>
-                    <v-flex xs12>
-                      <v-card v-for="(item, index) of items" :key="index">
-                        <v-card-title class="pt-1 pl-2 pb-1">
-                          <div class="subheading">{{ item.title }}</div>
-                        </v-card-title>
-                        <v-card-text class="px-2 pt-1">
-                          {{ item.subtitle }}
-                        </v-card-text>
-                        <div class="mb-3" v-if="index + 1 < items.length" :key="index"></div>
-                      </v-card>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-flex>
+            <boards-column></boards-column>
           </v-layout>
         </v-container>
       </v-card>
@@ -92,29 +24,12 @@
 </template>
 
 <script>
+import BoardsColumn from '~/components/BoardsColumn.vue'
+
 export default {
   name: 'boards',
-  data () {
-    return {
-      items: [
-        {
-          title: 'aiueo',
-          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
-        },
-        {
-          title: 'abcde',
-          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
-        },
-        {
-          title: 'kakuk',
-          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
-        },
-        {
-          title: 'sasisuseso',
-          subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.'
-        }
-      ]
-    }
+  components: {
+    'boards-column': BoardsColumn
   }
 }
 </script>
