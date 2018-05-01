eb8ce70a4c4358b00efa687f2a22d7c7be1e3260
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-05-02 01:29:32 +0900

    feat: mock pkt page

--- pages/kpt.vue
+++ pages/kpt.vue
@@ -1,3 +1,123 @@
 <template>
-  <p>KPT</p>
+  <div>
+    <v-layout row wrap>
+      <v-flex xs11 sm5>
+        <v-menu
+          ref="menu"
+          lazy
+          :close-on-content-click="false"
+          v-model="menu"
+          transition="scale-transition"
+          offset-y
+          full-width
+          :nudge-right="40"
+          min-width="290px"
+          :return-value.sync="date"
+        >
+          <v-text-field
+            slot="activator"
+            label="Picker in menu"
+            v-model="date"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-date-picker v-model="date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 md4>
+        <p class="display-2">KEEP</p>
+        <v-select
+          label="Your favorite hobbies"
+          chips
+          tags
+          solo
+          prepend-icon=""
+          append-icon=""
+          v-model="chips"
+        >
+          <template slot="selection" slot-scope="data">
+            <v-chip
+              close
+              @input="remove(data.item)"
+              :selected="data.selected"
+            >
+              <strong>{{ data.item }}</strong>&nbsp;
+              <span>(interest)</span>
+            </v-chip>
+          </template>
+        </v-select>
+      </v-flex>
+      <v-flex xs12 md4>
+        <p class="display-2">PROBLEM</p>
+        <v-select
+          label="Your favorite hobbies"
+          chips
+          tags
+          solo
+          prepend-icon=""
+          append-icon=""
+          v-model="chips"
+        >
+          <template slot="selection" slot-scope="data">
+            <v-chip
+              close
+              @input="remove(data.item)"
+              :selected="data.selected"
+            >
+              <strong>{{ data.item }}</strong>&nbsp;
+              <span>(interest)</span>
+            </v-chip>
+          </template>
+        </v-select>
+      </v-flex>
+      <v-flex xs12 md4>
+        <p class="display-2">TRY</p>
+        <v-select
+          label="Your favorite hobbies"
+          chips
+          tags
+          solo
+          prepend-icon=""
+          append-icon=""
+          v-model="chips"
+        >
+          <template slot="selection" slot-scope="data">
+            <v-chip
+              close
+              @input="remove(data.item)"
+              :selected="data.selected"
+            >
+              <strong>{{ data.item }}</strong>&nbsp;
+              <span>(interest)</span>
+            </v-chip>
+          </template>
+        </v-select>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: null,
+      menu: false,
+      chips: ['Programming', 'Playing video games', 'Watching', 'Sleeping']
+    }
+  },
+  methods: {
+    remove (item) {
+      this.chips.splice(this.chips.indexOf(item), 1)
+      this.chips = [...this.chips]
+    }
+  }
+}
+</script>
+
