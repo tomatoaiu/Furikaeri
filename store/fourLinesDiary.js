4b6626634782f44c7c1ba9ea095f2edd5490a56f
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-05-27 00:15:49 +0900

    feat: (fourLinesDiary) use actions of vuex

--- store/fourLinesDiary.js
+++ store/fourLinesDiary.js
@@ -16,32 +16,62 @@ export const state = () => ({
 })
 
 export const mutations = {
-  setFourLinesDiary (state, { date, content }) {
+  SET_FOUR_LINES_DIARY (state, { date, content }) {
     state.fourLinesDiary[date] = content
   },
-  setFact (state, { date, list }) {
+  SET_FACT (state, { date, list }) {
     state.fourLinesDiary[date].fact = list
   },
-  setAwareness (state, { date, list }) {
+  SET_AWARENESS (state, { date, list }) {
     state.fourLinesDiary[date].awareness = list
   },
-  setLesson (state, { date, list }) {
+  SET_LESSON (state, { date, list }) {
     state.fourLinesDiary[date].lesson = list
   },
-  setDeclaration (state, { date, list }) {
+  SET_DECLARATION (state, { date, list }) {
     state.fourLinesDiary[date].declaration = list
   },
-  removeFact (state, { date, item }) {
+  REMOVE_FACT (state, { date, item }) {
     state.fourLinesDiary[date].fact.splice(state.fourLinesDiary[date].fact.indexOf(item), 1)
   },
-  removeAwareness (state, { date, item }) {
+  REMOVE_AWARENESS (state, { date, item }) {
     state.fourLinesDiary[date].awareness.splice(state.fourLinesDiary[date].awareness.indexOf(item), 1)
   },
-  removeLesson (state, { date, item }) {
+  REMOVE_LESSON (state, { date, item }) {
     state.fourLinesDiary[date].lesson.splice(state.fourLinesDiary[date].lesson.indexOf(item), 1)
   },
-  removeDeclaration (state, { date, item }) {
+  REMOVE_DECLARATION (state, { date, item }) {
     state.fourLinesDiary[date].declaration.splice(state.fourLinesDiary[date].declaration.indexOf(item), 1)
+  }
+}
+
+export const actions = {
+  setFourLinesDiary ({ commit }, { date, content }) {
+    commit('SET_FOUR_LINES_DIARY', { date, content })
+  },
+  setFact ({ commit }, { date, list }) {
+    commit('SET_FACT', { date, list })
+  },
+  setAwareness ({ commit }, { date, list }) {
+    commit('SET_AWARENESS', { date, list })
+  },
+  setLesson ({ commit }, { date, list }) {
+    commit('SET_LESSON', { date, list })
+  },
+  setDeclaration ({ commit }, { date, list }) {
+    commit('SET_DECLARATION', { date, list })
+  },
+  removeFact ({ commit }, { date, item }) {
+    commit('REMOVE_FACT', { date, item })
+  },
+  removeAwareness ({ commit }, { date, item }) {
+    commit('REMOVE_AWARENESS', { date, item })
+  },
+  removeLesson ({ commit }, { date, item }) {
+    commit('REMOVE_LESSON', { date, item })
+  },
+  removeDeclaration ({ commit }, { date, item }) {
+    commit('REMOVE_DECLARATION', { date, item })
   }
 }
 
