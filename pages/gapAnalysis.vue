e6b682ab122df752f6665231c1042b3802f4edb4
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-09-16 14:10:56 +0900

    refactor: gap analysis

--- pages/gapAnalysis.vue
+++ pages/gapAnalysis.vue
@@ -1,232 +1,35 @@
 <template>
-  <div>
-    <v-layout row wrap>
-      <v-flex xs12 sm5>
-        <v-menu
-          ref="menu"
-          lazy
-          :close-on-content-click="false"
-          v-model="menu"
-          transition="scale-transition"
-          offset-y
-          full-width
-          :nudge-right="40"
-          min-width="290px"
-          :return-value.sync="date"
-          :color="baseColor"
-        >
-          <v-text-field
-            :color="baseColor"
-            slot="activator"
-            label="Picker in menu"
-            v-model="date"
-            prepend-icon="event"
-            readonly
-          ></v-text-field>
-          <v-date-picker
-            v-model="date"
-            no-title scrollable
-            :color="baseColor"
-            event-color="green"
-            :events="registerDates">
-            <v-spacer></v-spacer>
-            <v-btn flat :color="baseColor" @click="menu = false">Cancel</v-btn>
-            <v-btn flat :color="baseColor" @click="$refs.menu.save(date)">OK</v-btn>
-          </v-date-picker>
-        </v-menu>
-      </v-flex>
-      <v-spacer />
-      <v-flex xs3 sm3>
-        <v-btn flat icon :color="baseColor"
-         @click="toNextWeek">
-          <v-icon>keyboard_arrow_up</v-icon>
-        </v-btn>
-        <v-btn flat icon :color="baseColor"
-         @click="toLastWeek">
-          <v-icon>keyboard_arrow_down</v-icon>
-        </v-btn>
-        <v-btn flat icon :color="baseColor"
-         @click="toYesterday">
-          <v-icon>keyboard_arrow_left</v-icon>
-        </v-btn>
-        <v-btn flat icon :color="baseColor"
-          @click="toTomorrow">
-          <v-icon>keyboard_arrow_right</v-icon>
-        </v-btn>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap>
-      <v-flex xs12 md4>
-        <p class="display-2">ASIS</p>
-        <v-select
-          label="ASIS"
-          chips
-          tags
-          solo
-          prepend-icon=""
-          append-icon=""
-          v-model="gapAnalysisAsis"
-        >
-          <template slot="selection" slot-scope="data">
-            <v-chip
-              close
-              @input="removeAsis({ date, item: data.item })"
-              :selected="data.selected"
-              :color="gapAnalysisColor.asis"
-              text-color="white"
-            >
-              <v-avatar :color="gapAnalysisColor.asisAvatar">A</v-avatar>
-              <strong>{{ data.item }}</strong>
-            </v-chip>
-          </template>
-        </v-select>
-      </v-flex>
-      <v-flex xs12 md4>
-        <p class="display-2">TOBE</p>
-        <v-select
-          label="TOBE"
-          chips
-          tags
-          solo
-          prepend-icon=""
-          append-icon=""
-          v-model="gapAnalysisTobe"
-        >
-          <template slot="selection" slot-scope="data">
-            <v-chip
-              close
-              @input="removeTobe({ date, item: data.item })"
-              :selected="data.selected"
-              :color="gapAnalysisColor.tobe"
-              text-color="white"
-            >
-              <v-avatar :color="gapAnalysisColor.tobeAvatar">T</v-avatar>
-              <strong>{{ data.item }}</strong>
-            </v-chip>
-          </template>
-        </v-select>
-      </v-flex>
-      <v-flex xs12 md4>
-        <p class="display-2">GAP</p>
-        <v-select
-          label="GAP"
-          chips
-          tags
-          solo
-          prepend-icon=""
-          append-icon=""
-          v-model="gapAnalysisGap"
-        >
-          <template slot="selection" slot-scope="data">
-            <v-chip
-              close
-              @input="removeGap({ date, item: data.item })"
-              :selected="data.selected"
-              :color="gapAnalysisColor.gap"
-              text-color="white"
-            >
-              <v-avatar :color="gapAnalysisColor.gapAvatar">G</v-avatar>
-              <strong>{{ data.item }}</strong>
-            </v-chip>
-          </template>
-        </v-select>
-      </v-flex>
-    </v-layout>
-  </div>
+  <furikaeri
+    :name="name"
+    :model="furikaeri"
+    :word="word"
+    :color="furikaeriColor"
+    >
+  </furikaeri>
 </template>
 
 <script>
-import FurikaeriDate from '~/mixins/FurikaeriDate'
-import FurikaeriLocalStorage from '~/mixins/FurikaeriLocalStorage'
-import { mapGetters, mapActions } from 'vuex'
+import Furikaeri from '~/components/Furikaeri.vue'
+import { mapGetters } from 'vuex'
+
+const FURIKAERI = 'lamda'
+const WORD = ['look', 'ask', 'model', 'discuss', 'act']
 
 export default {
-  mixins: [ FurikaeriDate, FurikaeriLocalStorage ],
+  components: {
+    'furikaeri': Furikaeri
+  },
   data () {
     return {
-      menu: false,
-      date: undefined,
-      registerDates: undefined
+      name: FURIKAERI,
+      word: WORD
     }
   },
   computed: {
     ...mapGetters({
-      baseColor: 'color/base',
-      gapAnalysisColor: 'color/gapAnalysis',
-      gapAnalysis: 'gapAnalysis/gapAnalysis'
-    }),
-    gapAnalysisAsis: {
-      get () {
-        return this.getEachGapAnalysis('asis')
-      },
-      set (list) {
-        this.setAsis({ date: this.date, list })
-      }
-    },
-    gapAnalysisTobe: {
-      get () {
-        return this.getEachGapAnalysis('tobe')
-      },
-      set (list) {
-        this.setTobe({ date: this.date, list })
-      }
-    },
-    gapAnalysisGap: {
-      get () {
-        return this.getEachGapAnalysis('gap')
-      },
-      set (list) {
-        this.setGap({ date: this.date, list })
-      }
-    }
-  },
-  watch: {
-    date (next, prev) {
-      if (!this.hasDate(next)) {
-        this.setNewGapAnalysis(next)
-      }
-    }
-  },
-  mounted () {
-    this.initFurikaeriLocalStorage()
-    this.date = new Date().toJSON().slice(0, 10).replace(/-/g, '-')
-    if (!this.hasDate(this.date)) {
-      this.setNewGapAnalysis(this.date)
-    }
-    this.setRegisterDates()
-  },
-  methods: {
-    ...mapActions({
-      addGapAnalysis: 'gapAnalysis/addGapAnalysis',
-      setAsis: 'gapAnalysis/setAsis',
-      setTobe: 'gapAnalysis/setTobe',
-      setGap: 'gapAnalysis/setGap',
-      removeAsis: 'gapAnalysis/removeAsis',
-      removeTobe: 'gapAnalysis/removeTobe',
-      removeGap: 'gapAnalysis/removeGap'
-    }),
-    hasDate (date) {
-      if (date in this.gapAnalysis) {
-        return true
-      } else {
-        return false
-      }
-    },
-    getEachGapAnalysis (gapAnalysisWord) {
-      if (this.hasDate(this.date)) {
-        return this.gapAnalysis[this.date][gapAnalysisWord]
-      }
-    },
-    setNewGapAnalysis (date) {
-      this.addGapAnalysis({
-        date,
-        content: { asis: [], tobe: [], gap: [] }
-      })
-      this.setRegisterDates()
-    },
-    setRegisterDates () {
-      this.registerDates = Object.keys(this.gapAnalysis)
-    }
+      furikaeriColor: `color/${FURIKAERI}`,
+      furikaeri: `furikaeri/${FURIKAERI}`
+    })
   }
 }
 </script>
